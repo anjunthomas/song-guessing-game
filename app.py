47b0d293dff243b85e6558a0dda7a143d5906e3c
JessieLYeung <156261166+JessieLYeung@users.noreply.github.com>
2025-09-17 23:12:36 -0400

    Change /guess route to accept POST requests

--- app.py
+++ app.py
@@ -10,10 +10,16 @@ app = Flask(__name__)
 def hello():
     return "Hello World, this is the song guessing game made by Anju!"
 
+
+@app.route('/jessie')
+def jessie():
+    return "Hi this is Jessie"
 #backend developers, create a new route here with your name that returns a similar message
 # Ex route. '/anju' 
 # push to your branch to verify flask is working
-
+@app.route('/guess', methods=['POST'])
+def guess():
+    return "guess route is working"
 
 if __name__ == '__main__':
     app.run(debug=True)
